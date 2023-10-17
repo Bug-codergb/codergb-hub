27e5be276d6f24612625adcc2aefcf7384f14e39
codergb <betweent@126.com>
2023-10-17 21:36:43 +0800

    小程序首页

--- codergb-hub-uniapp/pages/detail/videoDetail/VideoDetail.vue
+++ codergb-hub-uniapp/pages/detail/videoDetail/VideoDetail.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<!-- <view ref="videoRef"></view> -->
+		<video :src="url"/>
 	</view>
 </template>
 <script>
@@ -20,38 +20,18 @@
 	import { getCurrentInstance,onMounted,ref } from "vue";
 	import { getVideoURL } from "../../../network/video/index.js";
 	import Hls from "hls.js";
-	import DPlayer from "dplayer";
+	//import DPlayer from "dplayer";
 	const url = ref("");
 	const videoRef = ref(null);
-	/*onMounted(()=>{
+	onMounted(()=>{
 		const instance = getCurrentInstance();
 		let id = instance.data.id;
 		getVideoURL(id).then((data)=>{
 			if(data.status===200){
 				url.value = data.data.url;
-				if(videoRef.value){
-					const dp = new DPlayer({
-					  container: videoRef.value,
-						autoplay:true,
-						loop:true,
-						preload:"ayto",
-						volume:0.7,
-					  video: {
-					    url: url.value,
-							type:"customHls",
-							customType:{
-								customHls:function(video,player){
-									const hls = new Hls();
-									hls.loadSource(video.src),
-									hls.attachMedia(video);
-								}
-							}
-					  },
-					});
-				}
 			}
 		})
-	})*/
+	})
 	
 </script>
 
--- codergb-hub-uniapp/pages/profile/Profile.vue
+++ codergb-hub-uniapp/pages/profile/Profile.vue
@@ -1,9 +1,75 @@
 <template>
-	profile
+	<view>
+		<view class="profile-header">
+			<view class="left-container">
+				<image class="avatar" :src="userInfo.avatarUrl" mode="widthFix"/>
+			</view>
+			<view class="right-container">
+				<text>{{userInfo.userName}}</text>
+			</view>
+		</view>
+		
+		<view class="user-operator">
+			<view class="item" v-for="item in userOperator" :key="item.label">
+				<uni-icons :type="item.type" size="30"></uni-icons>
+				<view>{{item.label}}</view>
+			</view>
+		</view>
+	</view>
 </template>
 
-<script>
+<script setup>
+	import { ref } from "vue";
+	import { LocalStorage } from "../../utils/storage.js"
+	let storage = new LocalStorage(false);
+	const userInfo = ref(storage.getItem('user')??{});
+	
+	const userOperator = ref([
+		{
+			label:"订阅内容",
+			type:"calendar"
+		},
+		{
+			label:"历史记录",
+			type:"map"
+		},
+		{
+			label:"稍后观看",
+			type:"navigate"
+		},
+		{
+			label:"顶过的视频",
+			type:"hand-up"
+		},
+	])
 </script>
-
-<style>
+<style scoped lang="less">
+	.profile-header{
+		display: flex;
+		align-items: center;
+		padding: 40rpx 30rpx;
+		.left-container{
+			width: 140rpx;
+			height: 140rpx;
+			border-radius: 50%;
+			overflow: hidden;
+			margin: 0 20rpx 0 0;
+			.avatar{
+				width: 100%;
+			}
+		}
+	}
+	.user-operator{
+		display: flex;
+		align-items: center;
+		padding: 20rpx 30rpx;
+		justify-content: space-between;
+		.item{
+			font-size: 26rpx;
+			cursor: pointer;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
 </style>
