d75d4879bc706a8f22c71756e96ffbd0f3daa7e4
codergb <betweent@126.com>
2023-12-15 23:19:03 +0800

    移动端

--- codergb-hub-client/src/views/shorts/childCpn/videoItem/style.tsx
+++ codergb-hub-client/src/views/shorts/childCpn/videoItem/style.tsx
@@ -10,6 +10,7 @@ export const VideoItemWrapper = styled.div`
     img {
       width: 100%;
       height: 100%;
+      vertical-align: bottom;
     }
     .bottom-level {
       width: 100%;
--- codergb-hub-client/src/views/shorts/index.tsx
+++ codergb-hub-client/src/views/shorts/index.tsx
@@ -28,7 +28,9 @@ const Shorts: FC = (): ReactElement => {
   const [video, setVideo] = useState<ICustomVideo[]>([]);
   const [total, setTotal] = useState<number>(0);
   const pending = useRef(false);
-  const offset = useRef(0);
+  const offset = useRef<number>(1);
+
+  const lastScrollTop = useRef<number>(0);
 
   const [direction, setDirection] = useState<string>("");
 
@@ -46,7 +48,7 @@ const Shorts: FC = (): ReactElement => {
     }
   };
   useEffect(() => {
-    getShortVideoReq(0, 3);
+    getShortVideoReq(0, 6);
   }, []);
   useEffect(() => {
     if (video.length !== 0) {
@@ -70,65 +72,110 @@ const Shorts: FC = (): ReactElement => {
   //   }
   // };
 
-  useEffect(() => {
-    const offsetHeight = listRef.current.offsetHeight;
-    let lastScrollTop = 0;
-    if (listRef && listRef.current) {
-      listRef.current.onscroll = lodash.debounce(
-        function (e: any) {
-          // if (t) {
-          //   clearTimeout(t);
-          // }
-          // t = setTimeout(() => {
-          //   if (!pending.current) {
-          //     scrollEndHandler(offsetHeight);
-          //   }
-          // }, 100);
-        },
-        1000,
-        false
-      );
-    }
-  }, [listRef.current]);
+  // useEffect(() => {
+  //   const offsetHeight = listRef.current.offsetHeight;
+  //   let lastScrollTop = 0;
+  //   if (listRef && listRef.current) {
+  //     listRef.current.onscroll = lodash.debounce(
+  //       function (e: any) {
+  //         if (t) {
+  //           clearTimeout(t);
+  //         }
+  //         t = setTimeout(() => {
+  //           if (!pending.current) {
+  //             scrollEndHandler(offsetHeight);
+  //           }
+  //         }, 100);
+  //       },
+  //       1000,
+  //       false
+  //     );
+  //   }
+  // }, [listRef.current]);
 
   const scrollRef = useRef<ICustomHtmlElement>(null);
   const moveSubScrollTop = () => {
+    console.log("sub sub sub");
+    scrollRef.current!.g_flag = false;
+    console.log(
+      listRef.current.scrollTop,
+      itemRef.current.offsetHeight * (offset.current - 2)
+    );
     requestAnimationFrame(() => {
-      listRef.current.scrollTop -= 20;
-      if (listRef.current.scrollTop === 0) {
-        pending.current = false;
+      if (
+        listRef.current.scrollTop >
+        itemRef.current.offsetHeight * (offset.current - 2)
+      ) {
+        let scrollTop = listRef.current.scrollTop;
+        listRef.current.scrollTop -= 20;
 
-        scrollRef.current!.g_flag = false;
+        if (scrollTop === listRef.current.scrollTop) {
+          debugger;
+        }
+        moveSubScrollTop();
+      } else {
+        listRef.current.scrollTop =
+          itemRef.current.offsetHeight * (offset.current - 2);
+        offset.current -= 1;
 
         const scrollHeight = scrollRef.current!.scrollHeight;
         const clientHeight = scrollRef.current!.clientHeight;
         scrollRef.current!.scrollTop = (scrollHeight - clientHeight) / 2;
-      } else {
-        moveSubScrollTop();
+
+        lastScrollTop.current = scrollRef.current!.scrollTop;
+        setDirection("");
       }
     });
   };
+
   const moveAddScrollTop = () => {
+    console.log("addd add");
+    scrollRef.current!.g_flag = false;
+
     requestAnimationFrame(() => {
-      if (listRef.current.scrollTop < itemRef.current.offsetHeight) {
-        listRef.current.scrollTop += 20;
-        moveAddScrollTop();
+      if (
+        listRef.current.scrollTop <
+        itemRef.current.offsetHeight * offset.current
+      ) {
+        let scrollTop = listRef.current.scrollTop;
+        listRef.current.scrollTop =
+          scrollTop + listRef.current.offsetHeight / 33;
+
+        if (scrollTop === listRef.current.scrollTop) {
+          //到达最底部
+          listRef.current.scrollTop =
+            itemRef.current.offsetHeight * offset.current;
+          offset.current += 1;
+
+          console.log(offset.current);
+          const scrollHeight = scrollRef.current!.scrollHeight;
+          const clientHeight = scrollRef.current!.offsetHeight;
+          scrollRef.current!.scrollTop = (scrollHeight - clientHeight) / 2;
+
+          lastScrollTop.current = scrollRef.current!.scrollTop;
+
+          setDirection("");
+        } else {
+          moveAddScrollTop();
+        }
       } else {
-        listRef.current.scrollTop = itemRef.current.offsetHeight;
-        pending.current = false;
-        scrollRef.current!.g_flag = false;
+        listRef.current.scrollTop =
+          itemRef.current.offsetHeight * offset.current;
+        offset.current += 1;
+
         const scrollHeight = scrollRef.current!.scrollHeight;
-        const clientHeight = scrollRef.current!.clientHeight;
+        const clientHeight = scrollRef.current!.offsetHeight;
         scrollRef.current!.scrollTop = (scrollHeight - clientHeight) / 2;
-        //debugger;
+
+        lastScrollTop.current = scrollRef.current!.scrollTop;
+        setDirection("");
       }
     });
   };
   useEffect(() => {
-    console.log(direction);
     if (direction === "向上") {
       moveAddScrollTop();
-    } else {
+    } else if (direction === "向下") {
       moveSubScrollTop();
     }
   }, [direction]);
@@ -136,30 +183,33 @@ const Shorts: FC = (): ReactElement => {
   let t: any = null;
   useEffect(() => {
     if (scrollRef.current) {
-      let lastScrollTop = 0;
-
+      lastScrollTop.current = 0;
       scrollRef.current.g_flag = true;
-      scrollRef.current.onscroll = function (e) {
+      scrollRef.current.addEventListener("scroll", function (e) {
         if (t) {
           clearTimeout(t);
         }
         t = setTimeout(() => {
           scrollRef.current!.g_flag = true;
+          console.log("end");
         }, 66);
         if (scrollRef.current!.g_flag) {
-          let scrollTop = (e.target! as HTMLDivElement).scrollTop;
-          if (scrollTop <= lastScrollTop) {
+          let scrollTop = scrollRef.current!.scrollTop;
+          console.log(lastScrollTop.current, scrollTop);
+          console.log((this as any).scrollTop);
+          console.log((e.target! as any).scrollTop);
+          if (scrollTop < lastScrollTop.current) {
             setDirection("向下");
             console.log("向下");
-          } else {
+          } else if (scrollTop > lastScrollTop.current) {
             setDirection("向上");
             console.log("向上");
           }
-          lastScrollTop = scrollTop;
+          lastScrollTop.current = scrollTop;
         }
-      };
+      });
     }
-  }, [scrollRef, scrollRef.current, pending.current]);
+  }, [scrollRef, scrollRef.current]);
   return (
     <ShortsWrapper>
       <div className="box">
